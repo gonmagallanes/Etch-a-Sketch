df454c4f103190ed38b3e2a5d1bdaac1a2516269
gonmagallanes <gonzalomagallanesnicola@gmail.com>
2025-06-25 20:06:49 -0300

    acomodado, falta emprolijar

--- js.js
+++ js.js
@@ -2,6 +2,109 @@ const container = document.querySelector("#container");
 const input = document.querySelector("#input");
 const button = document.querySelector("#button");
 const resetButton = document.querySelector("#reset");
+const rainbow = document.querySelector("#rainbow");
+const black = document.querySelector("#black");
+
+for (let i = 1; i <= 10; i++) {
+  const columns = document.createElement("section");
+  columns.setAttribute(
+    "style",
+    "display: flex; justify-content: center; align-items: center; width: 100%; height: 100%;"
+  );
+  for (let i = 1; i <= 10; i++) {
+    const rows = document.createElement("div");
+    rows.classList.add("paintBox");
+    rows.setAttribute(
+      "style",
+      " display: flex; flex: 1; height: 100%; border: 1px solid black;"
+    );
+    columns.appendChild(rows);
+  }
+  container.appendChild(columns);
+  container.setAttribute(
+    "style",
+    "display: flex; justify-content: center; align-items: center; border: 10px solid #96031A; margin: 5px; width: 35vw; height: 35vw; flex-direction: column;"
+  );
+  document.body.appendChild(container);
+}
+
+document.addEventListener("mousedown", () => {
+  isMouseDown = true;
+});
+
+document.addEventListener("mouseup", () => {
+  isMouseDown = false;
+});
+
+document.addEventListener("click", () => {
+  click = true;
+});
+
+const allBoxes = document.querySelectorAll(".paintBox");
+
+resetButton.addEventListener("click", () => {
+  allBoxes.forEach((box) => {
+    box.setAttribute(
+      "style",
+      "display: flex; flex: 1; height: 100%; border: 1px solid black;"
+    );
+  });
+});
+
+function randomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+}
+
+rainbow.addEventListener("click", () => {
+  allBoxes.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      if (isMouseDown) {
+        box.setAttribute(
+          "style",
+          `background-color: ${randomColor()}; display: flex; flex: 1; height: 100%; border: 1px solid black;`
+        );
+      }
+    });
+
+    allBoxes.forEach((box) => {
+      box.addEventListener("click", () => {
+        if (click) {
+          box.setAttribute(
+            "style",
+            `background-color: ${randomColor()}; display: flex; flex: 1; height: 100%; border: 1px solid black;`
+          );
+        }
+      });
+    });
+  });
+});
+
+black.addEventListener("click", () => {
+  allBoxes.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      if (isMouseDown) {
+        box.setAttribute(
+          "style",
+          `background-color: black; display: flex; flex: 1; height: 100%; border: 1px solid black;`
+        );
+      }
+    });
+  });
+
+  allBoxes.forEach((box) => {
+    box.addEventListener("click", () => {
+      if (click) {
+        box.setAttribute(
+          "style",
+          `background-color: black; display: flex; flex: 1; height: 100%; border: 1px solid black;`
+        );
+      }
+    });
+  });
+});
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
@@ -30,7 +133,7 @@ button.addEventListener("click", function (e) {
       container.appendChild(columns);
       container.setAttribute(
         "style",
-        "display: flex; justify-content: center; align-items: center; border: 1px solid black; margin: 5px; width: 500px; height: 500px; flex-direction: column;"
+        "display: flex; justify-content: center; align-items: center; border: 10px solid #96031A; margin: 5px; width: 35vw; height: 35vw; flex-direction: column;"
       );
       document.body.appendChild(container);
     }
@@ -65,25 +168,51 @@ button.addEventListener("click", function (e) {
       return `rgb(${r},${g},${b})`;
     }
 
-    allBoxes.forEach((box) => {
-      box.addEventListener("mouseenter", () => {
-        if (isMouseDown) {
-          box.setAttribute(
-            "style",
-            `background-color: ${randomColor()}; display: flex; flex: 1; height: 100%; border: 1px solid black; opacity: 0.7;`
-          );
-        }
+    rainbow.addEventListener("click", () => {
+      allBoxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
+          if (isMouseDown) {
+            box.setAttribute(
+              "style",
+              `background-color: ${randomColor()}; display: flex; flex: 1; height: 100%; border: 1px solid black;`
+            );
+          }
+        });
+
+        allBoxes.forEach((box) => {
+          box.addEventListener("click", () => {
+            if (click) {
+              box.setAttribute(
+                "style",
+                `background-color: ${randomColor()}; display: flex; flex: 1; height: 100%; border: 1px solid black;`
+              );
+            }
+          });
+        });
       });
     });
 
-    allBoxes.forEach((box) => {
-      box.addEventListener("click", () => {
-        if (click) {
-          box.setAttribute(
-            "style",
-            `background-color: ${randomColor()}; display: flex; flex: 1; height: 100%; border: 1px solid black;  opacity: 0.5;`
-          );
-        }
+    black.addEventListener("click", () => {
+      allBoxes.forEach((box) => {
+        box.addEventListener("mouseenter", () => {
+          if (isMouseDown) {
+            box.setAttribute(
+              "style",
+              `background-color: black; display: flex; flex: 1; height: 100%; border: 1px solid black;`
+            );
+          }
+        });
+      });
+
+      allBoxes.forEach((box) => {
+        box.addEventListener("click", () => {
+          if (click) {
+            box.setAttribute(
+              "style",
+              `background-color: black; display: flex; flex: 1; height: 100%; border: 1px solid black;`
+            );
+          }
+        });
       });
     });
   } else {
